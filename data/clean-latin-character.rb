f35a192130259221d42dcd7079f9a7c4df5ce54e
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2015-11-05 10:33:53 -0300

    Novos mapas

---
+++ data/clean-latin-character.rb
@@ -0,0 +1,9 @@
+file = File.read(ARGV[0])
+File.write(ARGV[0]+".backup", file)
+# http://stackoverflow.com/questions/225471/how-do-i-replace-accented-latin-characters-in-ruby
+file = file.tr(
+  "ÀÁÂÃÄÅàáâãäåĀāĂăĄąÇçĆćĈĉĊċČčÐðĎďĐđÈÉÊËèéêëĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħÌÍÎÏìíîïĨĩĪīĬĭĮįİıĴĵĶķĸĹĺĻļĽľĿŀŁłÑñŃńŅņŇňŉŊŋÒÓÔÕÖØòóôõöøŌōŎŏŐőŔŕŖŗŘřŚśŜŝŞşŠšſŢţŤťŦŧÙÚÛÜùúûüŨũŪūŬŭŮůŰűŲųŴŵÝýÿŶŷŸŹźŻżŽž",
+  "AAAAAAaaaaaaAaAaAaCcCcCcCcCcDdDdDdEEEEeeeeEeEeEeEeEeGgGgGgGgHhHhIIIIiiiiIiIiIiIiIiJjKkkLlLlLlLlLlNnNnNnNnnNnOOOOOOooooooOoOoOoRrRrRrSsSsSsSssTtTtTtUUUUuuuuUuUuUuUuUuUuWwYyyYyYZzZzZz"
+)
+File.write(ARGV[0], file)
+puts "create clean file from "+ARGV[0]
